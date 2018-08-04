46d8513c005aaf11a33d101b017a8eed0fcac23e
xiaozhongwen <xiaozhongwen@mye.hk>
2018-08-04 12:00:39 +0800

    创建单链表

---
+++ C++/List/SingleLinkList/SingleLinkList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SingleLinkList
+//
+//  Created by mye on 2018/8/4.
+//  Copyright © 2018 mye. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
